0c55dcb01a95404af399ce271dce10f167b78181
Zena Hirsch <hirsch.zena@gmail.com>
2015-08-09 14:07:03 -0400

    add device object and output method

--- littlecloud.js
+++ littlecloud.js
@@ -1,7 +1,54 @@
-exports.authorize = function (accessToken) {
-    var client = {
-        accessToken: accessToken
+var request = require('request');
+
+var Device = function (id, client) {
+    this.client = function () {
+        return client;
     };
 
-    return client;
+    this.id = function () {
+        return id;
+    };
+}
+
+Device.prototype.id = null
+Device.prototype.client = null;
+Device.prototype.output = function (percent, duration_ms) {
+    var options = {
+        url: 'https://api-http.littlebitscloud.cc/devices/' + this.id() + '/output',
+        method: 'POST',
+        json: true,
+        body: {
+            percent: percent,
+            duration_ms: duration_ms
+        },
+        headers: {
+            'Authorization': 'Bearer ' + this.client().accessToken(),
+            'Accept': 'application/vnd.littlebits.v2+json'  
+        }
+    };
+
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(body);
+        } else {
+            console.log(error);
+        }
+    });
+};
+
+
+var Client = function (accessToken) {
+    this.accessToken = function () {
+        return accessToken;
+    };
+};
+
+Client.prototype.accessToken = null;
+
+Client.prototype.makeDevice = function (id) {
+    return new Device(id, this);
+};
+
+module.exports = function (accessToken) {
+    return new Client(accessToken);
 };
